e34ccf99fb92c3c3586a256afae3fdb4b8b2dbd3
AKSHAY <fabdevil54@gmail.com>
2025-07-14 13:28:51 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,12 @@
 const firebaseConfig = {
-  apiKey: "YOUR-API-KEY",
-  authDomain: "YOUR-PROJECT-ID.firebaseapp.com",
-  databaseURL: "https://YOUR-PROJECT-ID.firebaseio.com",
-  projectId: "YOUR-PROJECT-ID",
-  storageBucket: "YOUR-PROJECT-ID.appspot.com",
-  messagingSenderId: "SENDER-ID",
-  appId: "APP-ID"
+  apiKey: "AIzaSyB0VwBfmpTy1T2W-2OPFpael7jBCeUQRSE",
+  authDomain: "inspire24-4cc84.firebaseapp.com",
+  databaseURL: "https://inspire24-4cc84-default-rtdb.firebaseio.com",
+  projectId: "inspire24-4cc84",
+  storageBucket: "inspire24-4cc84.firebasestorage.app",
+  messagingSenderId: "1062196864061",
+  appId: "1:1062196864061:web:d60b55044601205f5a3654",
+  measurementId: "G-MBZR5JKKWX"
 };
+
 firebase.initializeApp(firebaseConfig);
